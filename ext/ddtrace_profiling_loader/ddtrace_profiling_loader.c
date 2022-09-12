0712c67a1a59e4336039d7c623dcc5e27007c01d
Chris Lyon <chris.lyon@opendoor.com>
2022-09-12 12:53:09 -0700

    try ruby_xmalloc then fall back to ruby_xfree

--- ext/ddtrace_profiling_loader/ddtrace_profiling_loader.c
+++ ext/ddtrace_profiling_loader/ddtrace_profiling_loader.c
@@ -85,7 +85,14 @@ static bool failed_to_load(void *handle, VALUE *failure_details) {
 static bool incompatible_library(void *handle, VALUE *failure_details) {
   // The library being loaded may be linked to a different libruby than the current executing Ruby.
   // We check if this is the case by checking if a well-known symbol resolves to a common address.
-  if (dlsym(handle, "ruby_xfree") != &ruby_xfree) {
+  void *p_loaded_sym = dlsym(handle, "ruby_malloc");
+  void *p_ref_sym = &ruby_xmalloc;
+  if (ptr == NULL) {
+    p_loaded_sym = dlsym(handle, "ruby_xfree");
+    p_ref_sym = &ruby_xfree;
+  }
+
+  if (p_loaded_sym != p_ref_sym) {
     *failure_details = rb_str_new_cstr("library was compiled and linked to a different Ruby version");
     unload_failed_library(handle);
     return true;
